6720d149870595942a231f84ac34a5c1449fa178
fatma-12olmez <olmezfatma94@gmail.com>
2022-07-18 20:15:31 +0200

    third commit

--- src/myGitClass.java
+++ src/myGitClass.java
@@ -2,5 +2,7 @@ public class myGitClass {
 
     public static void main(String[] args) {
         System.out.println("I am learning Github!!");
+        System.out.println("I am adding new code...");
+        System.out.println("I am adding one more");
     }
 }
